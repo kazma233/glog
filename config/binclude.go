a9ce1d1af30b1a5e61c54c8b4676e4ba25c19457
kazmazeng <kazmazeng@gmail.com>
2020-07-25 17:12:38 +0000

    fix: update local env

--- config/binclude.go
+++ config/binclude.go
@@ -8,6 +8,6 @@ import (
 )
 
 var (
-	_binclude0	= []byte("mongo:\r\n  host: 192.168.1.129\r\n  port: 27017\r\n  username: admin\r\n  password: pw123456\r\n\r\n# PROD为正式\r\nenv: DEV\r\njwt-key: WC7fkchY6FL6eS")
-	BinFS		= binclude.FileSystem{"../assets": {Filename: "assets", Mode: 2147484159, ModTime: time.Unix(1593222293, 339024600), Compression: binclude.None, Content: nil}, "../assets/config.yml": {Filename: "config.yml", Mode: 438, ModTime: time.Unix(1593222293, 339000000), Compression: binclude.None, Content: _binclude0}}
+	_binclude0	= []byte("mongo:\n  host: 192.168.1.103\n  port: 27017\n  username: admin\n  password: pw123456\n\n# PROD为正式\nenv: DEV\njwt-key: WC7fkchY6FL6eS")
+	BinFS		= binclude.FileSystem{"../assets": {Filename: "assets", Mode: 2147484157, ModTime: time.Unix(1595688417, 81873061), Compression: binclude.None, Content: nil}, "../assets/config.yml": {Filename: "config.yml", Mode: 436, ModTime: time.Unix(1595693075, 509111664), Compression: binclude.None, Content: _binclude0}}
 )
--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"glog/config"
 	"log"
